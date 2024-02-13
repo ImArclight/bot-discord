ff66899c676b26ec6a988a2350e2da6523fa3709
FeiX <faisputra272@gmail.com>
2024-02-13 21:32:43 +0700

    Update responses.py

--- responses.py
+++ responses.py
@@ -3,13 +3,13 @@ from random import choice
 def get_response(user_input: str) -> str: 
    lowered: str = user_input.lower()
    
-   if lowered == '':
-       return 'hmmm?'
+   if lowered == 'ex':
+       return 'ex'
    elif 'hisui' in lowered:
-       return 'Yes, Master'
-   elif 'morning' in lowered:
-       return 'good morning, Master'
-   elif 'good night' in lowered:
-       return 'sleep well, Master'
+       return 'ex'
+   elif 'ex' in lowered:
+       return 'ex'
+   elif 'ex' in lowered:
+       return 'ex'
    else:
-       return '.....'
+       return 'ex'
